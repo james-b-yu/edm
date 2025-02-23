d2aec6ae7576afe21992583e4d49c23fb5fd4650
James Yu <fiercedeityproductions@outlook.com>
2025-02-23 18:19:06 +0000

    refactor to use dataclasses

--- data.py
+++ data.py
@@ -1,27 +1,72 @@
 import torch
 from torch.utils import data as td
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from functools import partial
 
-class DummyDataset(td.Dataset):
-    """Dummy dataset
+@dataclass
+class EDMDatasetItem:
+    """Attributes:
+        n_nodes (int): n, number of atoms in the molecule
+        coords (torch.Tensor): [n, 3] giving the 3D coordinates (x) of each atom in the molecule
+        features (torch.Tensor): [n, features_d] giving the features (h) of each atom in the molecule
     """
-    def __init__(self, features_d=7, len=10000, max_nodes=25):
-        """initialise dummy dataset
+    n_nodes: int
+    coords: torch.Tensor
+    features: torch.Tensor
+
+@dataclass
+class EDMDataloaderItem:
+    """
+    A class representing a batch of molecules. Note that we use "flattened" notation, in that coords and features are one long tensor of size [N, 3] or [N, features_d] respectively, where N is the number of atoms across the entire batch. This allows each molecule to have a different number of atoms and we keep track of this dynamically.
+    
+    We treat each batch as one single large "graph": each individual molecule in the batch has fully connected nodes. Therefore in order to represent the edges in the batch we only require tensors of outer length NN, where NN = (n_nodes ** 2).sum() is the number of edges in the batch.
+    
+    Attributes:
+        n_nodes (torch.Tensor): [B] where B is the batch size and each bth entry is the number of atoms in the bth molecule. Let N = n_nodes.sum() be the number of atoms in the entire batch and NN = (n_nodes ** 2).sum(), the number of edges in the batch.
+        coords (torch.Tensor): [N, 3], where N = n_nodes.sum(), giving the 3D coordinates of each atom in the batch
+        featuers (torch.Tensor): [N, features_d] giving the features of the each atom in the batch
+        edges (torch.Tensor): (NN, 2) tensor, where NN = (n_nodes ** 2).sum(). Each row of this tensor gives the indices of atoms within the batch for which there is a directed edge. Note that we do allow edges from a node to itself as this simplifies dealing with the ordering of the edges across the code
+        reduce (torch.Tensor): (N, NN) binary matrix where reduce[i, j] = 1 if and only if there is an edge from atom i->j
+    """
+    n_nodes: torch.Tensor  
+    coords: torch.Tensor  # [N, ]
+    features: torch.Tensor
+    edges: torch.Tensor
+    reduce: torch.Tensor
+
+class EDMDataset(ABC, td.Dataset):
+    def __init__(self, features_d=7, max_nodes=25):
+        """initialise EDM dataset
 
         Args:
             features_d (int, optional): number of data features. Defaults to 7.
-            len (int, optional): dummy dataset length. Defaults to 10000.
             max_nodes (int, optional): maximum number of atoms per molecule. Defaults to 25.
         """
         super().__init__()
         self.features_d = features_d
-        self.len = len
         self.max_nodes = max_nodes
+
+    @abstractmethod
+    def __len__(self) -> int:
+        pass
+
+    @abstractmethod
+    def __getitem__(self, index) -> EDMDatasetItem:
+        pass
+
+class DummyDataset(EDMDataset):
+    """Dummy dataset
+    """
+    def __init__(self, features_d=7, max_nodes=25, len=10000):
+        super().__init__(features_d, max_nodes)
+        self.len = len
         self.rng = torch.random
-        
-    def __len__(self):
+
+    def __len__(self) -> int:
         return self.len
-    
-    def __getitem__(self, index):
+
+    def __getitem__(self, index) -> EDMDatasetItem:
         """
         Args:
             index (int): the index
@@ -30,57 +75,37 @@ class DummyDataset(td.Dataset):
             dict[str, Any]: returns a dict containing "coords" and "n_nodes"
         """
         n_nodes = int(torch.randint(low=1, high=self.max_nodes + 1, size=(), dtype=torch.int64))
-        coords = torch.randn((n_nodes, 3), dtype=torch.float64)
-        features = torch.randn((n_nodes, self.features_d), dtype=torch.float64)
-        
-        if index == 0:
-            n_nodes = 2
-            coords = torch.tensor([[0, 0, 0],
-                                    [1, 1, 1]])
-        else:
-            n_nodes = 3
-            coords = torch.tensor([[0, 0, 0],
-                                    [1, 1, 1],
-                                    [2, 2, 2]])
-        
-        return {
-            "coords": coords,
-            "features": features,
-            "n_nodes": n_nodes
-        }
-        
-        
-        
-        
-        
-def get_dummy_dataloader(features_d: int, len: int, max_nodes: int, batch_size: int, device, use_sparse=True):
-    def collate_fn(data):
-        n_nodes = torch.tensor([d["n_nodes"] for d in data], dtype=torch.int64)  # (B, ) tensor where each bth entry is the number of atoms in the bth molecule
-        coords = torch.cat([d["coords"] for d in data])  # (N, 3) tensor, where N = n_nodes.sum(), and each nth entry is the 3D coordinate of the nth atom in the batch
-        features = torch.cat([d["features"] for d in data])  # (N, features_d) tensor, where N = n_nodes.sum(), and each nth entry is the 3D coordinate of the nth atom in the batch
-        
+        coords = torch.randn((n_nodes, 3), dtype=torch.float32)
+        features = torch.randn((n_nodes, self.features_d), dtype=torch.float32)
+
+        return EDMDatasetItem(n_nodes=n_nodes, coords=coords, features=features)
+
+def _collate_fn(data: list[EDMDatasetItem], use_sparse: bool, device: torch.device):
+        n_nodes = torch.tensor([d.n_nodes for d in data], dtype=torch.int64)
+        coords = torch.cat([d.coords for d in data])
+        features = torch.cat([d.features for d in data])
+
         _n_nodes_cumsum = torch.cumsum(torch.cat([torch.tensor([0], dtype=torch.int64), n_nodes]), dim=0)
-        edges = torch.cat([torch.cartesian_prod(torch.arange(b_n.item(), dtype=torch.int64), torch.arange(b_n.item(), dtype=torch.int64)) + _n_nodes_cumsum[b] for b, b_n in enumerate(n_nodes)], dim=0)  # (NN, 2) tensor, where NN = (n_nodes ** 2).sum(). Each row of this tensor gives the indices of atoms within the batch for which there is a directed edge. Note that we do allow edges from a node to itself as this simplifies the ordering of the edges. (Otherwise when dealing with indices of edges, we must keep track of which pairs are from an atom to itself which is more complicated)
-        reduce = torch.block_diag(*[torch.block_diag(*[torch.ones((int(n), )).scatter_(0, torch.tensor([m]), 0) for m in range(n)]) for n in n_nodes])  # (N, NN) binary matrix where reduce[i, j] = 1 if and only if there is an edge from atom i->j
         
+        edges = torch.cat([torch.cartesian_prod(torch.arange(b_n.item(), dtype=torch.int64), torch.arange(b_n.item(), dtype=torch.int64)) + _n_nodes_cumsum[b] for b, b_n in enumerate(n_nodes)], dim=0) 
+        reduce = torch.block_diag(*[torch.block_diag(*[torch.ones((int(n), )).scatter_(0, torch.tensor([m]), 0) for m in range(n)]) for n in n_nodes])
+
         # get them all on the relevant device
         n_nodes = n_nodes.to(device)
         coords  = coords.to(device)
         features = features.to(device)
         edges = edges.to(device)
         reduce: torch.Tensor = reduce.to(device)
-        
+
         if use_sparse:
             reduce = reduce.to_sparse_csr()
-        
+
         _xe = coords[edges]
-        
-        return {
-            "coords": coords,
-            "features": features,
-            "n_nodes": n_nodes,
-            "edges": edges,
-            "reduce": reduce
-        }
-    
+
+        return EDMDataloaderItem(n_nodes=n_nodes, coords=coords, features=features, edges=edges, reduce=reduce)
+
+def get_dummy_dataloader(features_d: int, len: int, max_nodes: int, batch_size: int, device, use_sparse=True):
+
+    collate_fn = partial(_collate_fn, use_sparse=use_sparse, device=device)
+
     return td.DataLoader(dataset=DummyDataset(features_d, len, max_nodes), batch_size=batch_size, collate_fn=collate_fn)
--- model.py
+++ model.py
@@ -1,51 +1,59 @@
+from typing import Any
 import torch
 from torch import nn
-from typing import TypeGuard
+from dataclasses import dataclass
+from data import EDMDataloaderItem
+
+@dataclass
+class EGCLConfig:
+    features_d: int
+    node_attr_d: int
+    edge_attr_d: int
+    hidden_d: int
+
+@dataclass
+class EGNNConfig(EGCLConfig):
+    num_layers: int
 
 class EGCL(nn.Module):
-    def __init__(self, features_d: int, node_attr_d: int, edge_attr_d: int, output_d: int, hidden_d: int, act_fn = nn.SiLU()):
+    def __init__(self, config: EGCLConfig):
         super().__init__()
-        
+
         # the \(\phi_{e}\) network for edge operation
         self.edge_mlp = nn.Sequential(
             #            hi               hj            dij2         aij
-            nn.Linear(features_d   +   features_d   +   1   +   edge_attr_d, hidden_d),
-            act_fn,
-            nn.Linear(hidden_d, hidden_d),
-            act_fn
+            nn.Linear(config.features_d   +   config.features_d   +   1   +   config.edge_attr_d, config.hidden_d),
+            nn.SiLU(),
+            nn.Linear(config.hidden_d, config.hidden_d),
+            nn.SiLU()
         )
-        
+
         # the \(\phi_{x}\) network for coordinate update (same architecture as above but we have an additional projection onto a scalar)
         self.coord_mlp = nn.Sequential(
             #            hi               hj            dij2         aij
-            nn.Linear(features_d   +   features_d   +   1   +   edge_attr_d, hidden_d),
-            act_fn,
-            nn.Linear(hidden_d, hidden_d),
-            act_fn,
-            nn.Linear(hidden_d, 1)
+            nn.Linear(config.features_d   +   config.features_d   +   1   +   config.edge_attr_d, config.hidden_d),
+            nn.SiLU(),
+            nn.Linear(config.hidden_d, config.hidden_d),
+            nn.SiLU(),
+            nn.Linear(config.hidden_d, 1)
         )
-        
+
         # the \(\phi_{h}\) network for node update
         self.node_mlp = nn.Sequential(
-            nn.Linear(features_d + hidden_d + node_attr_d, hidden_d),
-            act_fn,
-            nn.Linear(hidden_d, output_d)
+            nn.Linear(config.features_d + config.hidden_d + config.node_attr_d, config.hidden_d),
+            nn.SiLU(),
+            nn.Linear(config.hidden_d, config.features_d)
         )
-        
+
         # the \(\phi_{inf}\) network for edge inference operator
         self.inf_mlp = nn.Sequential(
-            nn.Linear(hidden_d, 1),
+            nn.Linear(config.hidden_d, 1),
             nn.Sigmoid()
         )
-        
+
         # save things
-        self.features_d = features_d
-        self.node_attr_d = node_attr_d
-        self.edge_attr_d = edge_attr_d
-        self.output_d = output_d
-        self.hidden_d = hidden_d
-        self.act_fn = act_fn
-        
+        self.config = config
+
     def forward(self, coords: torch.Tensor, features: torch.Tensor, edges: torch.Tensor, reduce: torch.Tensor, node_attr: torch.Tensor|None=None, edge_attr: torch.Tensor|None=None):
         """forward pass for equivariant graph convolutional layer
 
@@ -57,23 +65,25 @@ class EGCL(nn.Module):
             node_attr (torch.Tensor | None, optional): Node attributes. None (if using no node attributes), or [N, node_attr_d]. Defaults to None.
             edge_attr (torch.Tensor | None, optional): Edge attributes. None (if using no edge attributes), or [NN, node_attr_d]. Defaults to None.
         """
-        
+
         x_e = coords[edges]  # [NN, 2, 3]
         h_e = features[edges]  # [NN, 2, features_d]
         h_e_flattened = h_e.flatten(start_dim=1)  # [NN, 2 * features_d]
         d_e = (x_e[:, 0, :] - x_e[:, 1, :]).norm(dim=1).unsqueeze(dim=1) # [NN, 1]
-        
+
         # calculate output for the \(\phi_{e}\) and \(\phi_{x}\) networks
-        coord_mlp_in = edge_mlp_in = torch.cat([x_e, h_e_flattened, d_e ** 2], dim=-1) if edge_attr is None else torch.cat([x_e, h_e_flattened, d_e ** 2, edge_attr], dim=-1)
+        coord_mlp_in = edge_mlp_in = torch.cat([h_e_flattened, d_e ** 2] + ([edge_attr] if edge_attr is not None else []), dim=-1)
         phi_e = self.edge_mlp(edge_mlp_in)  # [NN, hidden_d]
         phi_x = self.coord_mlp(coord_mlp_in)   # [NN, 1]
-        
+
         # calculate output for the \(\phi_{h}\) network, giving feature vector output
-        node_mlp_in = torch.cat([features, reduce @ (phi_e * self.inf_mlp(phi_e))], dim=-1) if node_attr is None else torch.cat([features, reduce @ (phi_e * self.inf_mlp(phi_e)), node_attr], dim=-1)
-        features_out: torch.Tensor = self.node_mlp(node_mlp_in)
-        
+        node_mlp_in = torch.cat([features, reduce @ (phi_e * self.inf_mlp(phi_e))] + ([node_attr] if node_attr is not None else []), dim=-1)
+        features_out: torch.Tensor = features + self.node_mlp(node_mlp_in)  # [N, features_d]
+
         # calculate coord vector output
-        w = phi_x / (d_e + 1)
-        coords_out: torch.Tensor = coords + reduce @ (w * (x_e[:, 0, :] - x_e[:, 1, :]))
-        
-        return features_out, coords_out
+        w = phi_x / (d_e + 1)  # [NN, 1]
+        coords_out: torch.Tensor = coords + reduce @ (w * (x_e[:, 0, :] - x_e[:, 1, :]))  # [N, 3]
+
+        return coords_out, features_out
+
+
