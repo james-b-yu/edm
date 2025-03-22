d47d9111fd24aced6985247305c79c6b58809d7a
James Yu <fiercedeityproductions@outlook.com>
2025-03-22 15:12:28 +0000

    fix the import

--- extensions/regularization/disconnection_penalty.py
+++ extensions/regularization/disconnection_penalty.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 from collections import deque
 from extensions.regularization import compute_bonds
-from configs.datasets_config import get_dataset_info
+from configs.dataset_reg_config import get_dataset_info
 
 def bfs(start, A):
     queue = deque()
